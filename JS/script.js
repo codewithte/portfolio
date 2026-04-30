8fa463d102a7fd4930ea73fb550a68fd4d3a9202
codewithte <codewithte@gmail.com>
2026-04-30 14:09:36 +0700

    done project

--- JS/script.js
+++ JS/script.js
@@ -65,16 +65,96 @@ closeIcons.addEventListener("click",()=>{
 })
 
 const iconJS = document.querySelector('.js');
+const barPercent = document.querySelector('.skill-container');
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach(entry =>{
         if(entry.isIntersecting){
-            entry.target.classList.add('rotatejs');
+            if(entry.target.classList.contains('skill-container')){
+                const animations = document.querySelectorAll('.bar-percentage');
+                animations.forEach(Animation =>{
+                    Animation.classList.add('animation');
+                })
+            } else if(entry.target.classList.contains('js')){
+                entry.target.classList.add('rotatejs');
+            }
         } else{
             entry.target.classList.remove('rotatejs');
         }
     });
-})
+}, {threshold: 0.5})
 observer.observe(iconJS);
+observer.observe(barPercent);   
+
+
+//Generate Project
+const project = [
+    {title:"CRUD Form",img:"crudForm.png",Des:"POS System with create, update, delete, add",lang:["Boostrap","Javascript"], linkCode: "https://github.com/codewithte/CRUD_Form", linkDemo: "https://crud-form-beta.vercel.app/"},
+    {title:"E-Commerce",img:"shop.png",Des:"E-Commerce platform: Selling Product",lang:["HTML","CSS"], linkCode: "https://github.com/codewithte/TeTech", linkDemo: "https://codewithte.github.io/TeTech/"},
+    {title:"Form",img:"crudForm.png",Des:"POS System with create, update, delete, add",lang:["Boostrap","Javascript","Angular"], linkCode: "https://github.com/codewithte/CRUD_Form", linkDemo: "https://crud-form-beta.vercel.app/"},
+    {title:"Form",img:"crudForm.png",Des:"POS System with create, update, delete, add",lang:["Boostrap","Javascript","Angular"], linkCode: "https://github.com/codewithte/CRUD_Form", linkDemo: "https://crud-form-beta.vercel.app/"}
+];
+const projectContainer = document.querySelector('.project-container');
+project.forEach(p=>{
+    const project = document.createElement("div");
+    project.classList.add('project');
+    const lang = p.lang.map(language => `<span>${language}</span>`).join("");
+
+    project.innerHTML = `
+        <div class="pic"><img src="images/project/${p.img}"></div>
+        <div class="projectDetail">
+            <div class="projectTitle">${p.title}</div>
+            <div class="projectDes">${p.Des}</div>
+            <div class="projectLang">
+                ${lang}
+            </div>
+            <div class="projectBtn">
+                <a href="${p.linkCode}" target="_blank" rel="noopener noreferrer">
+                    <i class="fa-brands fa-github"></i>Code
+                </a>
+                <a href="${p.linkDemo}" target="_blank" rel="noopener noreferrer">
+                    <i class="fa-solid fa-circle-play"></i>Live Demo
+                </a>
+            </div>
+        </div>
+        </div>
+    `;
+    projectContainer.appendChild(project);
+})
+
+const skills = [
+    {lang:"HTML",img:"html.png", percent:"75%", color:'barColor1'},
+    {lang:"CSS",img:"css.png", percent:"65%", color:'barColor2'},
+    {lang:"Javascript",img:"js.png", percent:"45%", color:'barColor3'},
+    {lang:"Typescript",img:"ts.png", percent:"10%", color:'barColor4'},
+    {lang:"Angular",img:"angular.png", percent:"8%", color:'barColor5'},
+    {lang:"Vite",img:"Vite.png", percent:"0%", color:'barColor6'},
+    {lang:"Tailwinds",img:"tailwind.png", percent:"20%", color:'barColor7'},
+    {lang:"React",img:"react.png", percent:"2%", color:'barColor8'}
+];
+const skillContainer = document.querySelector('.skill-container');
+skills.forEach(skill=>{
+    const container = document.createElement('div');
+    container.innerHTML=`
+        <div class="header-skill">
+            <div class="logo-skill"><img src="images/skill-logo/${skill.img}"></div>
+            <div class="text-skill">${skill.lang}</div>
+        </div>
+        <div class="body-skill" style="--percent: ${skill.percent};">
+            <div class="skill-bar">
+                <div class="proficiency">Proficiency</div>
+                <div class="percentage" >${skill.percent}</div>
+            </div>
+            <div class="bar-percentage ${skill.color}"></div>
+        </div>
+    `;
+    skillContainer.appendChild(container);
+})
+
+
+
+
+
+
 
 
 
